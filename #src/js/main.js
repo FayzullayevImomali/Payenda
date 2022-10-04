783fba1df62825eb28485d064ba81f88e7f9259c
Imomali <fayzullayevimomali@mail.ru>
2022-10-04 22:55:07 +0500

    fourth commit

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,9 +1,8 @@
-'use strict';
-
 const search = document.querySelector('.search');
 const hiddenForm = document.querySelector('.hidden');
 const gamburger = document.querySelector('.gamburger');
 const navbarContent = document.querySelector('.navbar-content');
+const dropDown = document.querySelectorAll('.dropdown-btn');
 
 search.addEventListener('click', () => {
     hiddenForm.classList.toggle('active');
@@ -12,6 +11,27 @@ search.addEventListener('click', () => {
 gamburger.addEventListener('click', () => {
     gamburger.classList.toggle('active-gamburger');
     navbarContent.classList.toggle('navbar-content-active');
-})
+});
 
+dropDown.forEach((btns) => {
+    btns.addEventListener('click', (e) => {
+        e.preventDefault;
+        const dropdown = e.currentTarget.nextElementSibling;
+        dropdown.classList.toggle('dropdown-list-active');
 
+        document.querySelectorAll('.dropdown-list + .dropdown-btn').forEach((element ) => {
+            if(element !== e.currentTarget.nextElementSibling) {
+                dropdown.classList.remove('dropdown-list-active');
+            }
+        });
+    });
+});
+
+window.onclick = (e) => {
+    if(!e.target.matches('.dropdown-list')) {
+        const dropdowns = document.querySelectorAll('.dropdown-list');
+        dropdowns.forEach((btns) => {
+            btns.classList.remove('dropdown-list-active');
+        });
+    }
+};
