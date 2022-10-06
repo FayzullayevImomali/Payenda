b58e0819a7118ecabf0a8f30ca0157f5f2decbee
Imomali <fayzullayevimomali@mail.ru>
2022-10-06 18:20:29 +0500

    nthchanges

--- Payenda/js/main.js
+++ Payenda/js/main.js
@@ -3,6 +3,10 @@ const hiddenForm = document.querySelector('.hidden');
 const gamburger = document.querySelector('.gamburger');
 const navbarContent = document.querySelector('.navbar-content');
 const dropDown = document.querySelectorAll('.dropdown-btn');
+const dropdownContent = document.querySelector('.dropdown-list');
+const firstLink  = document.querySelector('.dropdown-btn1');
+const secondLink = document.querySelector('.dropdown-btn2');
+const arr = ['apple', 'limon', 'banan']
 
 search.addEventListener('click', () => {
     hiddenForm.classList.toggle('active');
@@ -13,6 +17,13 @@ gamburger.addEventListener('click', () => {
     navbarContent.classList.toggle('navbar-content-active');
 });
 
+// firstLink.addEventListener('click', (e) => {
+//    dropdownContent.classList.toggle('dropdown-list-active');
+//    if(secondLink.includes('')) {
+//     console.log('not includes');
+//    }
+// });
+
 
 dropDown.forEach((btns) => {
     btns.addEventListener('click', (e) => {
@@ -20,9 +31,19 @@ dropDown.forEach((btns) => {
         const dropdown = e.currentTarget.nextElementSibling;
         dropdown.classList.toggle('dropdown-list-active');
 
-        
+        document.querySelector(".dropdown-btn + .dropdown-list").forEach((dropdown) => {
+            if(dropdown !== e.currentTarget.nextElementSibling) {
+                dropdown.classList.remove('dropdown-list-active');
+            }
+        });
     });
 });
+
+
+console.log(arr.includes('apple'));
+
+
+
 
 
 
